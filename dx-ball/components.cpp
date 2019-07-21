50283aafe987fff4864d13fd6cbdeea99be4a4b4
Naveen <naveen.j_94@outlook.com>
2019-07-21 15:36:54 +0200

    Fix circular dependency and add collision checks

--- dx-ball/components.cpp
+++ dx-ball/components.cpp
@@ -10,18 +10,74 @@ using namespace sf;
 Ball::Ball(float x, float y, float radius): 
     Item(x, y, radius, radius), CircleShape(radius) 
 {
-    this->m_motionVector = make_pair(1,1);
+    this->m_motionVector = make_pair(MOTION, MOTION);
     this->setPosition(x,y);
 }
 
-void Ball::update() {
+void Ball::update(Game *game) {
+    deflectUpdate(game);
     CircleShape::move(this->m_motionVector.first, this->m_motionVector.second);
 }
 
-bool Ball::isCollision() {
+void Ball::deflectUpdate(Game *game) {
+    
     // check collision with the bases
-    // check collision with the bottom/top
+    for(Base* base: game->m_bases) {
+        if (this->checkCollision(base)) {
+            if (base->getPosition().y > game->getHeight()/2)
+                m_motionVector.second = -MOTION;
+            else
+                m_motionVector.second = MOTION;
+        }
+    }
+
     // check collision with the sides
+    std::pair<bool, bool> collision_result = checkWallCollision(game);
+    if (collision_result.first)
+        m_motionVector.first = MOTION;
+    else if (collision_result.second)
+        m_motionVector.first = -MOTION;
+
+
+    // check collision with the bottom/top
+}
+
+// bool Ball::checkWallCollision(Game *game) {
+
+//     // game->getWidth()
+//     sf::Rect<int> bounds(0, game->getHeight(), game->getWidth(), 4);
+//     sf::Rect<int> ballShape(getPosition().x, getPosition().y, m_dimension.first, m_dimension.second);
+
+//     bool didIntersect = bounds.intersects(ballShape);
+//     cout << "bool: " << didIntersect << endl;
+//     return didIntersect;
+// }
+
+std::pair<bool,bool> Ball::checkWallCollision(Game *game) {
+
+    bool intersect_left = false, intersect_right = false;
+    sf::Rect<int> ballShape(getPosition().x, getPosition().y, m_dimension.first, m_dimension.second);
+     // TODO: create this just once in the beginning
+    sf::Rect<int> bounds_right(game->getWidth(), 0, 5, game->getHeight());
+    sf::Rect<int> bounds_left(-5, 0, 5, game->getHeight());
+
+    if (bounds_right.intersects(ballShape)) {
+        return make_pair(false, true);
+    }
+    else if (bounds_left.intersects(ballShape)) {
+        return make_pair(true, false);
+    }
+    return make_pair(false, false);
+}
+
+bool Ball::checkCollision(Base *targetShape) {
+    
+    sf::Rect<int> ballShape(getPosition().x, getPosition().y, m_dimension.first, m_dimension.second);
+    sf::Rect<int> otherShape(targetShape->getPosition().x, targetShape->getPosition().y, targetShape->m_dimension.first, targetShape->m_dimension.second);
+
+    bool didIntersect = ballShape.intersects(otherShape);
+
+    return didIntersect;
 }
 
 Brick::Brick(float x, float y, float width, float height):
--- dx-ball/components.hpp
+++ dx-ball/components.hpp
@@ -7,24 +7,16 @@
 
 // Game includes
 #include "item.hpp"
+// #include "game.hpp"
+#include "components.hpp"
+
 
 namespace game {
-    
-    class Ball: public Item, public sf::CircleShape {
-        private:
-        TwoDim m_motionVector;
-        public:
-        Ball(float x, float y, float radius);
-        void update();
-        bool isCollision();
-    };
 
-    class Brick: public Item, public sf::RectangleShape {
-        private:
-        bool m_present = true;
-        public:
-        Brick(float x, float y, float width, float height);
-    };
+    class Game;
+    class Ball;
+    class Brick;
+    class Base;
 
     class Base: public Item, public sf::RectangleShape {
         private:
@@ -35,4 +27,25 @@ namespace game {
         void left();
         Base(float x, float y, float width, float height);
     };
+    
+    class Ball: public Item, public sf::CircleShape {
+        private:
+        const float MOTION = 0.2;
+        TwoDim m_motionVector;
+        public:
+        Ball(float x, float y, float radius);
+        void update(Game *game);
+        bool isCollision();
+        void deflectUpdate(Game *game);
+        bool checkCollision(Base *targetShape);
+        std::pair<bool,bool> checkWallCollision(Game *game);
+    };
+
+    class Brick: public Item, public sf::RectangleShape {
+        private:
+        bool m_present = true;
+        public:
+        Brick(float x, float y, float width, float height);
+    };
+
 }
