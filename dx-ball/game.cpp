a4d779726a9f75db9cf511d5a63c772d3b76f135
Naveen <naveen.j_94@outlook.com>
2019-07-21 12:14:44 +0200

    Add game files

--- dx-ball/game.cpp
+++ dx-ball/game.cpp
@@ -1,5 +1,7 @@
 #include "game.hpp"
 
+#include <iostream>
+
 using namespace game;
 using namespace sf;
 
@@ -21,6 +23,10 @@ const int Game::getHeight() {
 void Game::addBase() {
     const int BASE_HEIGHT = 20, BASE_WIDTH = 70 ;
     m_base = new Base(getWidth()/2 - BASE_WIDTH/2, getHeight() - BASE_HEIGHT, BASE_WIDTH, BASE_HEIGHT);
+
+    m_bases.push_back(new Base(getWidth()/2 - BASE_WIDTH/2, getHeight() - BASE_HEIGHT, BASE_WIDTH, BASE_HEIGHT));
+    m_bases.push_back(new Base(getWidth()/2 - BASE_WIDTH/2, BASE_HEIGHT, BASE_WIDTH, BASE_HEIGHT));
+
     // m_drawables.push_back(&m_base);
 }
 
@@ -34,12 +40,109 @@ void Game::draw() {
     //     m_windowHandle.draw(each_brick);
     // }
 
-    m_windowHandle.draw(*m_base);
+    for (const Base *each_base: m_bases) {
+        m_windowHandle.draw(*each_base);
+    }
+
+    // m_windowHandle.draw(*m_base);
     m_windowHandle.draw(*m_ball);
     // m_windowHandle.draw(m_ball);
+}
+
+void Game::addBrick() {
+
+    // int rows = 1, columns = 5;
+
+    // int width = getWidth()/columns;
+
+    // m_bricks.push_back(new Brick());
+}
+
+void Game::update() {
+    m_ball->update();
+    m_bases[0]->update();
+    m_bases[1]->update();
 }
 
 Game::~Game() {
 
 }
 
+void Game::input() {
+    Event event;
+
+    while (m_windowHandle.pollEvent(event)) {
+
+        if(event.type == sf::Event::Closed) 
+                m_windowHandle.close();
+
+        if (event.type == Event::KeyPressed) {
+
+            switch (event.key.code) {
+
+                case Keyboard::Escape:
+                    m_windowHandle.close();
+                    break;
+
+                case Keyboard::Right:
+                    eventTrigger(g_event_t::right);
+                    break;
+                    
+                case Keyboard::Left:
+                    eventTrigger(g_event_t::left);
+                    break;
+            }
+        }
+
+    }
+}
+
+void Game::eventTrigger(g_event_t event) {
+    switch(event) {
+        case g_event_t::right:
+            eventRight();
+            break;
+        case g_event_t::left:
+            eventLeft();
+            break;
+    }
+}
+
+// void Game::Run() {
+
+//      while (m_windowHandle.isOpen()) {
+        
+//         Event event;
+
+//         while (m_windowHandle.pollEvent(event)) {
+            
+
+//         }
+
+//         gameHandle.m_windowHandle.clear();
+//         gameHandle.update();
+//         gameHandle.draw();
+
+//         // gameHandle.
+//         // gameHandle.m_windowHandle.draw(shape);
+//         // gameHandle.m_windowHandle.draw(brick_shape);
+//         // gameHandle.m_windowHandle.draw(paddle_shape);
+//         gameHandle.m_windowHandle.display();
+//     }
+// }
+
+void Game::eventRight() {
+    m_bases[0]->right();
+    m_bases[1]->right();
+}
+
+void Game::eventLeft() {
+    m_bases[0]->left();
+    m_bases[1]->left();
+}
+
+bool Game::isInFrame(Vector2f point) {
+    if (point.x <= 1000 && point.y <= 1000 && point.x >= 0 && point.y >= 0)
+        return true;
+    return false;
+}
--- dx-ball/game.hpp
+++ dx-ball/game.hpp
@@ -1,23 +1,54 @@
+#pragma once
+
 #include <string>
 #include <utility>
+#include <vector>
 
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
 
 #include "item.hpp"
+#include "components.hpp"
 
 namespace game {
+
+    enum class g_event_t {
+        right,
+        left
+    };
 
     class Game {
 
         private:
         std::string m_title;
         TwoDim m_dimension;
+        std::vector<Base*> m_bases;
+        Base *m_base;
+        Ball *m_ball;
+        std::vector<Brick*> m_bricks;
+        // std::vector<Brick*> m_drawables;
         
         public:
+        static bool isInFrame(sf::Vector2f point);
         sf::RenderWindow m_windowHandle;
         Game(int width, int height, std::string title);
         TwoDim getDimension();
         const int getWidth();
         const int getHeight();
+        void addBase();
+        void addBrick();
+        void addBall();
+
+        void init();
+        void input();
+        void update();
+        void draw();
+        void Run();
+
+        void eventRight();
+        void eventLeft();
+        void eventTrigger(g_event_t eventHandler);
+        ~Game();
+
     };
 }
