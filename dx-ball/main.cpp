65c6c7c8f88eac7931bae7b490538a0ff0613bba
Naveen <naveen.j_94@outlook.com>
2019-07-21 12:39:30 +0200

    Use functions

--- dx-ball/main.cpp
+++ dx-ball/main.cpp
@@ -15,26 +15,23 @@ int main() {
     gameHandle.addBase();
     gameHandle.addBall();
 
+    gameHandle.addBrick();
+
+    sf::RectangleShape shape(sf::Vector2f(2,2));
+
+    // shape.move()
+
     // rshape.setFillColor(sf::Color::Green);
     // shape.setFillColor(sf::Color::Green);
 
     while (gameHandle.m_windowHandle.isOpen()) {
+
+        gameHandle.input();
         
-        sf::Event event;
-
-        while (gameHandle.m_windowHandle.pollEvent(event)) {
-            if (((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape))
-                || (event.type == sf::Event::Closed)) 
-            {
-                gameHandle.m_windowHandle.close();
-            }
-        }
-
         gameHandle.m_windowHandle.clear();
+        gameHandle.update();
         gameHandle.draw();
-        // gameHandle.m_windowHandle.draw(shape);
-        // gameHandle.m_windowHandle.draw(brick_shape);
-        // gameHandle.m_windowHandle.draw(paddle_shape);
+
         gameHandle.m_windowHandle.display();
     }
 }
